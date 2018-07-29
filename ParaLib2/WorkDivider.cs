bbe3f6ba8c86626e1f0d0a69eaf97b5722b0269c
Maxadinara <Maxadinara@gmail.com>
2018-07-29 23:46:14 +0200

    Constructor added to WorkDivider.

--- ParaLib2/WorkDivider.cs
+++ ParaLib2/WorkDivider.cs
@@ -8,9 +8,24 @@ namespace ParaLib2
         private Map map;
         private Worker[] worker;
 
+        public WorkDivider(int listSize, int threadCount)
+        {
+            this.listSize = listSize;
+        }
+
         public void ExecuteThreadWork(Object worker)
         {
            Worker work = (Worker)worker;
+        }
+
+        private void InlizielizeWorkers(int threadCount)
+        {
+            worker = new Worker[listSize];
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                worker[i] = new Worker();
+            }
         }
     }
 }
